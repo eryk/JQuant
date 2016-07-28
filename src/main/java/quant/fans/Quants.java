032abcb70ed5044c9e7491cdb801da314b271440
牧仁 <xuqi.xq@alibaba-inc.com>
2016-07-28 10:03:37 +0800

    JQuant使用实例

--- src/main/java/quant/fans/Quants.java
+++ src/main/java/quant/fans/Quants.java
@@ -3,6 +3,7 @@ package quant.fans;
 import quant.fans.model.StockData;
 import quant.fans.provider.Provider;
 import quant.fans.strategy.StrategyUtils;
+import quant.fans.tools.StockList;
 
 import java.util.List;
 import java.util.Map;
@@ -24,6 +25,7 @@ public class Quants {
      */
     public final StrategyUtils strategy = new StrategyUtils();
 
+    public final StockList stocks = new StockList();
     /**
      * 使用外部存储，支持：
      * 文件系统(csv)
@@ -36,32 +38,5 @@ public class Quants {
     public Quants db(Map<String, String> config) {
         //TODO
         return this;
-    }
-
-    public static void main(String[] args) {
-        Quants quants = new Quants();
-        List<String> list = quants.data.stockList();
-        quants.data.tradingStockList();
-
-        for (String stock : list) {
-            List<StockData> stockDatas = quants.data.dailyData(stock);
-            if(stockDatas == null || stockDatas.size() < 60){
-                continue;
-            }
-            quants.indicator.macd(stockDatas);
-            quants.indicator.sma(stockDatas);
-            quants.indicator.boll(stockDatas);
-            quants.indicator.kdj(stockDatas);
-            quants.strategy.macdCross(stockDatas);
-            quants.strategy.kdjCross(stockDatas);
-            quants.strategy.goldenSpider(stockDatas);
-            quants.strategy.bollThroat(stockDatas);
-            for (StockData stockData : stockDatas) {
-                if (stockData.get("boll_throat") > 0) {
-                    System.out.println(stockData);
-                }
-            }
-        }
-
     }
 }
